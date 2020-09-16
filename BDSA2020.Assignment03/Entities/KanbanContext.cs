80493d210a7b21d878c8bd86d08a77d15f55094e
tjoms tjomson <thhk@itu.dk>
2020-09-16 11:53:47 +0200

    Fix import

--- BDSA2020.Assignment03/Entities/KanbanContext.cs
+++ BDSA2020.Assignment03/Entities/KanbanContext.cs
@@ -1,6 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace BDSA2020.Assignment03.Entities
 {
-    public class KanbanContext
+    public class KanbanContext : DbContext
     {
     }
 }
--- BDSA2020.Assignment03/Entities/Task.cs
+++ BDSA2020.Assignment03/Entities/Task.cs
@@ -2,5 +2,14 @@ namespace BDSA2020.Assignment03.Entities
 {
     public class Task
     {
+        public int Id {get; set;}
+        public string Title {get; set;}
+        public User? AssignedTo {get; set;}
+        public string? Description {get; set;}
+        public State State {get; set;}
+    }
+
+    public enum State{
+        NEW,ACTIVE,RESOLVED,CLOSED,REMOVED
     }
 }
