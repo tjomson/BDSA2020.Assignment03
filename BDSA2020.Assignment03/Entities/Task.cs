79b2e7d4086e043b42411716201bf679e88c311d
tjoms tjomson <thhk@itu.dk>
2020-09-17 14:14:53 +0200

    Add stringlength and required constraints

--- BDSA2020.Assignment03/Entities/Task.cs
+++ BDSA2020.Assignment03/Entities/Task.cs
@@ -1,12 +1,19 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace BDSA2020.Assignment03.Entities
 {
     public class Task
     {
         public int Id {get; set;}
+        [Required]
+        [StringLength(100)]
         public string Title {get; set;}
         public User? AssignedTo {get; set;}
         public string? Description {get; set;}
+        [Required]
         public State State {get; set;}
+        public ICollection<Tag> Tags {get; set;}
     }
 
     public enum State{
--- BDSA2020.Assignment03/Entities/User.cs
+++ BDSA2020.Assignment03/Entities/User.cs
@@ -1,6 +1,19 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
 namespace BDSA2020.Assignment03.Entities
 {
     public class User
     {
+        public int Id {get; set;}
+        [Required]
+        [StringLength(100)]
+        public string Title {get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Email {get; set; }
+        public ICollection<Task> Tasks {get; set; }
+
     }
 }
